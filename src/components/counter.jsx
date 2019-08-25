025e0142f9baa2391d78c8c7ce1d791767f1a3e7
hamzaakhtar <hamza.akhtar@arbisoft.com>
2019-08-25 23:02:56 +0500

    Added NavBar component, item count and decrement count functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
+    const { onIncrement, onDecrement, onDelete, counter } = this.props;
     return (
       <React.Fragment>
         <span className={this.getClassName()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
-          className="btn btn-secondary btn-sm"
+          onClick={() => onIncrement(counter)}
+          className="btn btn-secondary btn-sm m-2"
         >
-          Increment
+          +
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDecrement(counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          --
+        </button>
+        <button
+          onClick={() => onDelete(counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -32,12 +31,12 @@ class Counter extends Component {
 
   getClassName() {
     let classLabels = "badge m-2 badge-";
-    classLabels += this.state.value === 0 ? "warning" : "primary";
+    classLabels += this.props.counter.value === 0 ? "warning" : "primary";
     return classLabels;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
