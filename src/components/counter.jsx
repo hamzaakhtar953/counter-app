a272a8b7cac8cd675e4ea6e82c32764093d67aa9
hamzaakhtar <hamza.akhtar@arbisoft.com>
2019-08-25 19:20:13 +0500

    Added Delete button and logic for reset button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.counter.value
   };
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -15,24 +14,31 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getClassName()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        <br />
       </React.Fragment>
     );
   }
 
   getClassName() {
     let classLabels = "badge m-2 badge-";
-    classLabels += this.state.count === 0 ? "warning" : "primary";
+    classLabels += this.state.value === 0 ? "warning" : "primary";
     return classLabels;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
